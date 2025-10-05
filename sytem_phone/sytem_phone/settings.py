830704d32b038f0cc6de499d35a4ed7438ed0b8f
CodeAriCos28 <arafaelacosta28@gmail.com>
2025-10-04 23:48:38 -0400

    arregle la fecha

--- sytem_phone/sytem_phone/settings.py
+++ sytem_phone/sytem_phone/settings.py
@@ -144,12 +144,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+# En settings.py
+TIME_ZONE = 'America/Santo_Domingo'  # Zona horaria de República Dominicana
+USE_TZ = True  # Usar zonas horarias
 USE_I18N = True
 
-USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
